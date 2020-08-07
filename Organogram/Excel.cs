011f5d8e2a58e8bd13f5645be41b05c994e12fdd
lukasz-mosiezny <65019369+adam-mickiewicz@users.noreply.github.com>
2020-08-07 12:14:40 +0200

    extract Person to another file

--- Organogram/Excel.cs
+++ Organogram/Excel.cs
@@ -104,31 +104,4 @@ namespace Organogram
         }
 
     }
-
-    public class Person
-    {
-        public int id;
-        public int parentID;
-        public string name;
-        public string surname;
-        public string company;
-        public string city;
-        public string position;
-        public double firstNumber;
-        public double secondNumber;
-        public double thirdNumber;
-        public Person()
-        {
-            id = 0;
-            parentID = 0;
-            name = "";
-            surname = "";
-            company = "";
-            city = "";
-            position = "";
-            firstNumber = 0;
-            secondNumber = 0;
-            thirdNumber = 0;
-        }
-    }
 }
---
+++ Organogram/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Organogram
+{
+    public class Person
+    {
+        public int id;
+        public int parentID;
+        public string name;
+        public string surname;
+        public string company;
+        public string city;
+        public string position;
+        public double firstNumber;
+        public double secondNumber;
+        public double thirdNumber;
+        public Person()
+        {
+            id = 0;
+            parentID = 0;
+            name = "";
+            surname = "";
+            company = "";
+            city = "";
+            position = "";
+            firstNumber = 0;
+            secondNumber = 0;
+            thirdNumber = 0;
+        }
+    }
+}
